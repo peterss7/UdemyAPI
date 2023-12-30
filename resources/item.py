69416c5f096ab9d37575e381807e7e684cf50db1
peterss7 <peterss7405926@gmail.com>
2023-12-30 17:02:33 -0500

    Getting sqlalchemy set up

--- resources/item.py
+++ resources/item.py
@@ -1,13 +1,16 @@
-import uuid
 from flask.views import MethodView
 from flask_smorest import abort, Blueprint
-from db import items
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+
+from db import db
+from models import ItemModel
 from schemas import ItemSchema, ItemUpdateSchema
 
 blp = Blueprint("items", __name__)
 
 @blp.route("/items/<string:item_id>")
 class Item(MethodView):
+    @blp.response(200, ItemSchema)
     def get(self, item_id):
         try:
             return items[item_id]
@@ -17,11 +20,12 @@ class Item(MethodView):
     def delete(self, item_id):
         try:
             del items[item_id]
-            return "Item Deleted", 204
+            return "Item Deleted"
         except KeyError:
             abort(404, message="Item not found")
             
     @blp.arguments(ItemUpdateSchema)
+    @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
         try:
             item = items[item_id]
@@ -39,7 +43,12 @@ class ItemList(MethodView):
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data):
-        item_id = uuid.uuid4().hex
-        item = { **item_data, "id": item_id }
-        items[item_id] = item
-        return item, 201
+        item = ItemModel(**item_data)
+        
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="Could not add item to database")
+        
+        return item
